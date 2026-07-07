04089b2dde8444c4b36a5d0da597108937ef0834
Denzel Chingodza <denzel.chingodza@icloud.com>
2026-07-07 18:51:48 +0200

    Init project structure and first two files config and sentinel connector

---
+++ app/config.py
@@ -0,0 +1,11 @@
+import os
+
+#backend urls set here
+SENTINEL_API_URL = os.getenv("SENTINEL_API_URL", "")
+STACKSCOPE_API_URL = os.getenv("STACKSCOPE_API_URL", "")
+DOCUZEN_API_URL = os.getenv("DOCUZEN_API_URL", "")
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
+
+ALLOWED_ORIGINS =os.getenv("ALLOWED_ORIGINS", "*")
+
